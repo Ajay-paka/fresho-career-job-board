9e80b834a20cad8b574d83ca2db3c49124bb530a
Ajay-paka <ajaypaka47023@gmail.com>
2026-02-13 17:55:18 +0530

    fixed port

--- app.py
+++ app.py
@@ -261,7 +261,8 @@ def edit_job(job_id):
 
 # ---------------- RUN ---------------- #
 
+import os
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-    
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
 #app.run(debug=True)
